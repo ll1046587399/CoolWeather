d04e1e70a07db9257d866857aa3ab111c53da3d6
luolei <1046587399@qq.com>
2017-02-19 21:46:29 +0800

    显示天气信息

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by lei on 2017/2/18.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
